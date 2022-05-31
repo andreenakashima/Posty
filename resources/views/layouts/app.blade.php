748e3afd0a670e3818359eaa270d2dbe6fdccdbb
andreenakashima <andreenakashima@gmail.com>
2022-05-31 11:14:41 -0300

    Added login and logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
+
             <li>
                 <a href="" class="p-3">Home</a>
             </li>
@@ -21,19 +22,27 @@
                 <a href="" class="p-3">Post</a>
             </li>
         </ul>
+
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Andre</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Andre</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
 
