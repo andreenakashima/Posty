f78d45e354fe59dbcb47a048f3e2db896cf98591
andreenakashima <andreenakashima@gmail.com>
2022-06-01 15:53:51 -0300

    Added authorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,13 +30,13 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb2">{{ $post->body }}</p>
 
-                        <div>
+                        @can('delete', $post)
                             <form action="{{ route('posts.destroy', $post) }}" method="POST" class="mr-1">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-blue-500">Delete</button>
                             </form>
-                        </div>
+                        @endcan
 
                         <div class="flex items-center">
                             @auth
