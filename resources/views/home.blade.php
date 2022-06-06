245ba31e2e542793873b483d5ef874b66d04f764
andreenakashima <andreenakashima@gmail.com>
2022-06-06 19:56:23 -0300

    teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Home 2
+            Home
         </div>
     </div>
 @endsection
